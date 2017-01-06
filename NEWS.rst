b619f7ed2a4da707ab458ee17708332385ede05f
Allen Li <darkfeline@felesatra.moe>
2017-01-05 21:50:46 -0800

    Bump version

--- NEWS.rst
+++ NEWS.rst
@@ -3,6 +3,9 @@ Release notes
 
 This project uses `semantic versioning <http://semver.org/>`_.
 
+master
+------
+
 0.2.0 (2017-01-05)
 ------------------
 
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup
 
 setup(
     name='mir.sqlqs',
-    version='0.2.0',
+    version='0.3.0',
     description='Relational SQL API (SQL QuerySet)',
     long_description='',
     keywords='',
